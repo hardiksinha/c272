3a77fb4c89c3916d23b334677af39ae0015a095b
Hardik <hardik.sinha04@gmail.com>
2024-02-05 20:51:42 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,47 @@
+import os
+
+from flask import Flask, request, jsonify, render_template, redirect, url_for,send_file
+from faker import Faker
+from twilio.jwt.access_token import AccessToken
+from twilio.jwt.access_token.grants import SyncGrant
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+fake = Faker()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Add your Twilio credentials
+@app.route('/token')
+def generate_token():
+    TWILIO_ACCOUNT_SID = 'AC569177be929bf89f1444e284ba0ebd8d'
+    TWILIO_SYNC_SERVICE_SID = 'ISd7c3d0c4cf7dfab44937d08d7f4ac819'
+    TWILIO_API_KEY = 'SK3c7e4e2e5ddd4dfb485e03e2bbdcd2f0'
+    TWILIO_API_SECRET = 'ZXcWnEuuKIotkB4BP7D9Zq5JKAMlLwDK'
+
+    username = request.args.get('username', fake.user_name())
+
+    # create access token with credentials
+    token = AccessToken(TWILIO_ACCOUNT_SID, TWILIO_API_KEY, TWILIO_API_SECRET, identity=username)
+    # create a Sync grant and add to token
+    sync_grant_access = SyncGrant(TWILIO_SYNC_SERVICE_SID)
+    token.add_grant(sync_grant_access)
+    return jsonify(identity=username, token=token.to_jwt().decode())
+
+# A function to download text and store it in text file
+@app.route('/', methods=['POST'])
+def download_text():
+    text_from_notepad = request.form['text']
+    with open('workfile.txt', 'w') as f:
+        f.write(text_from_notepad)
+
+    path_to_store_txt = "workfile.txt"
+
+    return send_file(path_to_store_txt, as_attachment=True)
+
+
+if __name__ == "__main__":
+    app.run(host='localhost', port='5001', debug=True)
